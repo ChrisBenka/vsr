29e02fc0dbe7fd25cc29f5e3aae44a32df41f924
ChrisBenka <chrisbenka@gmail.com>
2024-05-05 14:50:13 -0400

    Update edge_enhancer.py

--- codes/models/edge_enhancer.py
+++ codes/models/edge_enhancer.py
@@ -29,6 +29,209 @@ class EdgeExtraction(Enum):
 
 from torch import nn
 import torch.nn.functional as F
+#https://github.com/DCurro/CannyEdgePytorch/blob/master/net_canny.py
+def get_gaussian_kernel(k=3, mu=0, sigma=1, normalize=True):
+    # compute 1 dimension gaussian
+    gaussian_1D = np.linspace(-1, 1, k)
+    # compute a grid distance from center
+    x, y = np.meshgrid(gaussian_1D, gaussian_1D)
+    distance = (x ** 2 + y ** 2) ** 0.5
+
+    # compute the 2 dimension gaussian
+    gaussian_2D = np.exp(-(distance - mu) ** 2 / (2 * sigma ** 2))
+    gaussian_2D = gaussian_2D / (2 * np.pi *sigma **2)
+
+    # normalize part (mathematically)
+    if normalize:
+        gaussian_2D = gaussian_2D / np.sum(gaussian_2D)
+    return gaussian_2D
+    #https://github.com/DCurro/CannyEdgePytorch/blob/master/net_canny.py
+
+def get_sobel_kernel(k=3):
+    # get range
+    range = np.linspace(-(k // 2), k // 2, k)
+    # compute a grid the numerator and the axis-distances
+    x, y = np.meshgrid(range, range)
+    sobel_2D_numerator = x
+    sobel_2D_denominator = (x ** 2 + y ** 2)
+    sobel_2D_denominator[:, k // 2] = 1  # avoid division by zero
+    sobel_2D = sobel_2D_numerator / sobel_2D_denominator
+    return sobel_2D
+    #https://github.com/DCurro/CannyEdgePytorch/blob/master/net_canny.py
+
+def get_thin_kernels(start=0, end=360, step=45):
+    k_thin = 3  # actual size of the directional kernel
+    # increase for a while to avoid interpolation when rotating
+    k_increased = k_thin + 2
+
+    # get 0° angle directional kernel
+    thin_kernel_0 = np.zeros((k_increased, k_increased))
+    thin_kernel_0[k_increased // 2, k_increased // 2] = 1
+    thin_kernel_0[k_increased // 2, k_increased // 2 + 1:] = -1
+
+    # rotate the 0° angle directional kernel to get the other ones
+    thin_kernels = []
+    for angle in range(start, end, step):
+        (h, w) = thin_kernel_0.shape
+        # get the center to not rotate around the (0, 0) coord point
+        center = (w // 2, h // 2)
+        # apply rotation
+        rotation_matrix = cv2.getRotationMatrix2D(center, angle, 1)
+        kernel_angle_increased = cv2.warpAffine(thin_kernel_0, rotation_matrix, (w, h), cv2.INTER_NEAREST)
+
+        # get the k=3 kerne
+        kernel_angle = kernel_angle_increased[1:-1, 1:-1]
+        is_diag = (abs(kernel_angle) == 1)  # because of the interpolation
+        kernel_angle = kernel_angle * is_diag  # because of the interpolation
+        thin_kernels.append(kernel_angle)
+    return thin_kernels
+
+class CannyFilter(nn.Module):
+    #https://github.com/DCurro/CannyEdgePytorch/blob/master/net_canny.py
+    def __init__(self,
+                 k_gaussian=3,
+                 mu=0,
+                 sigma=1,
+                 k_sobel=3,
+                 use_cuda=True):
+        super(CannyFilter, self).__init__()
+        # device
+        self.device = 'cuda' if use_cuda else 'cpu'
+
+        # gaussian
+
+        gaussian_2D = get_gaussian_kernel(k_gaussian, mu, sigma)
+        self.gaussian_filter = nn.Conv2d(in_channels=1,
+                                         out_channels=1,
+                                         kernel_size=k_gaussian,
+                                         padding=k_gaussian // 2,
+                                         bias=False)
+        for param in self.gaussian_filter.parameters():
+            param.requires_grad = False
+
+        self.gaussian_filter.weight[:] = torch.from_numpy(gaussian_2D)
+
+        # sobel
+
+        sobel_2D = get_sobel_kernel(k_sobel)
+        self.sobel_filter_x = nn.Conv2d(in_channels=1,
+                                        out_channels=1,
+                                        kernel_size=k_sobel,
+                                        padding=k_sobel // 2,
+                                        bias=False)
+        for param in self.sobel_filter_x.parameters():
+            param.requires_grad = False
+        self.sobel_filter_x.weight[:] = torch.from_numpy(sobel_2D)
+
+
+        self.sobel_filter_y = nn.Conv2d(in_channels=1,
+                                        out_channels=1,
+                                        kernel_size=k_sobel,
+                                        padding=k_sobel // 2,
+                                        bias=False)
+        for param in self.sobel_filter_y.parameters():
+            param.requires_grad = False
+
+        self.sobel_filter_y.weight[:] = torch.from_numpy(sobel_2D.T)
+
+
+        # thin
+
+        thin_kernels = get_thin_kernels()
+        directional_kernels = np.stack(thin_kernels)
+
+        self.directional_filter = nn.Conv2d(in_channels=1,
+                                            out_channels=8,
+                                            kernel_size=thin_kernels[0].shape,
+                                            padding=thin_kernels[0].shape[-1] // 2,
+                                            bias=False)
+        for param in self.directional_filter.parameters():
+            param.requires_grad = False
+        self.directional_filter.weight[:, 0] = torch.from_numpy(directional_kernels)
+
+        # hysteresis
+
+        hysteresis = np.ones((3, 3)) + 0.25
+        self.hysteresis = nn.Conv2d(in_channels=1,
+                                    out_channels=1,
+                                    kernel_size=3,
+                                    padding=1,
+                                    bias=False)
+        for param in self.hysteresis.parameters():
+            param.requires_grad = False
+        self.hysteresis.weight[:] = torch.from_numpy(hysteresis)
+
+
+    def forward(self, img, low_threshold=None, high_threshold=None, hysteresis=False):
+        # set the setps tensors
+        B, C, H, W = img.shape
+        blurred = torch.zeros((B, C, H, W)).to(self.device)
+        grad_x = torch.zeros((B, 1, H, W)).to(self.device)
+        grad_y = torch.zeros((B, 1, H, W)).to(self.device)
+        grad_magnitude = torch.zeros((B, 1, H, W)).to(self.device)
+        grad_orientation = torch.zeros((B, 1, H, W)).to(self.device)
+
+        # gaussian
+
+        for c in range(C):
+            blurred[:, c:c+1] = self.gaussian_filter(img[:, c:c+1])
+
+            grad_x = grad_x + self.sobel_filter_x(blurred[:, c:c+1])
+            grad_y = grad_y + self.sobel_filter_y(blurred[:, c:c+1])
+
+        # thick edges
+
+        grad_x, grad_y = grad_x / C, grad_y / C
+        grad_magnitude = (grad_x ** 2 + grad_y ** 2) ** 0.5
+        grad_orientation = torch.atan(grad_y / grad_x)
+        grad_orientation = grad_orientation * (360 / np.pi) + 180 # convert to degree
+        grad_orientation = torch.round(grad_orientation / 45) * 45  # keep a split by 45
+
+        # thin edges
+
+        directional = self.directional_filter(grad_magnitude)
+        # get indices of positive and negative directions
+        positive_idx = (grad_orientation / 45) % 8
+        negative_idx = ((grad_orientation / 45) + 4) % 8
+        thin_edges = grad_magnitude.clone()
+        # non maximum suppression direction by direction
+        for pos_i in range(4):
+            neg_i = pos_i + 4
+            # get the oriented grad for the angle
+            is_oriented_i = (positive_idx == pos_i) * 1
+            is_oriented_i = is_oriented_i + (positive_idx == neg_i) * 1
+            pos_directional = directional[:, pos_i]
+            neg_directional = directional[:, neg_i]
+            selected_direction = torch.stack([pos_directional, neg_directional])
+
+            # get the local maximum pixels for the angle
+            is_max = selected_direction.min(dim=0)[0] > 0.0
+            is_max = torch.unsqueeze(is_max, dim=1)
+
+            # apply non maximum suppression
+            to_remove = (is_max == 0) * 1 * (is_oriented_i) > 0
+            thin_edges[to_remove] = 0.0
+
+        # thresholds
+
+        if low_threshold is not None:
+            low = thin_edges > low_threshold
+
+            if high_threshold is not None:
+                high = thin_edges > high_threshold
+                # get black/gray/white only
+                thin_edges = low * 0.5 + high * 0.5
+
+                if hysteresis:
+                    # get weaks and check if they are high or not
+                    weak = (thin_edges == 0.5) * 1
+                    weak_is_high = (self.hysteresis(thin_edges) > 1) * weak
+                    thin_edges = high * 1 + weak_is_high * 1
+            else:
+                thin_edges = low * 1
+
+
+        return  thin_edges
 
 
 class EdgeDetector(torch.nn.Module):
@@ -57,6 +260,7 @@ class EdgeDetector(torch.nn.Module):
         self.weight_h = nn.Parameter(data=kernel_h, requires_grad=False)
         self.weight_v = nn.Parameter(data=kernel_v, requires_grad=False)
         self.laplac = nn.Parameter(data=laplacian_kernel_detail, requires_grad=False)
+        self.canny = CannyFilter()
         self.type = type
 
     def normalize(self, x):
@@ -83,6 +287,8 @@ class EdgeDetector(torch.nn.Module):
             x = F.conv2d(sobel, self.laplac, padding=1)
         elif self.type == "LAPLACIAN":
             x = F.conv2d(x, self.laplac, padding=1)
+        elif self.type == "CannyFilter":
+            x = self.canny(x)
         else:
             x_v = F.conv2d(x, self.weight_v, padding=1)
             x_h = F.conv2d(x, self.weight_h, padding=1)
@@ -177,7 +383,6 @@ class Mask(nn.Module):
         return x
 
 
-
 class EdgeEnhancement(nn.Module):
     def __init__(self, edge_enhancement_type="LAPLACIAN"):
         super(EdgeEnhancement, self).__init__()
@@ -185,12 +390,12 @@ class EdgeEnhancement(nn.Module):
 
         self.leaky = nn.LeakyReLU(negative_slope=.2)
 
-        self.encoder_1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1)
-        self.encoder_2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1)
+        self.encoder_1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1,padding='same')
+        self.encoder_2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1,padding='same')
         self.encoder_3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=2,padding=1)
-        self.encoder_4 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1)
+        self.encoder_4 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1,padding='same')
         self.encoder_5 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=2,padding=1)
-        self.encoder_6 = nn.Conv2d(in_channels=256, out_channels=64, kernel_size=3, stride=1)
+        self.encoder_6 = nn.Conv2d(in_channels=256, out_channels=64, kernel_size=3, stride=1,padding='same')
 
         # intermediate conv layer.
         self.conv_tranpose_interm = nn.Sequential(
@@ -220,7 +425,7 @@ class EdgeEnhancement(nn.Module):
             self.leaky
         )
 
-        self.conv_final = nn.ConvTranspose2d(in_channels=64, out_channels=1, kernel_size=3, padding=1)
+        self.conv_final = nn.ConvTranspose2d(in_channels=64, out_channels=3, kernel_size=3, padding=1)
 
     def encoder(self, frames):
         x = self.leaky(self.encoder_1(frames))
@@ -270,36 +475,10 @@ class EdgeEnhancement(nn.Module):
         return np.stack(hr_seq).transpose(0, 2, 3, 1), \
             np.stack(edge_seq).transpose(0, 2, 3, 1)  # thwc
 
-    ### --- NEEDED ASSISTANCE TO MATCH COLOR / LUMINENANCE IN IMAGE I GENERATED TO WHAT WAS EXPECTED
-    """NOT MY CODE """
-    def rgb_to_ycbcr(self, image):
-        r, g, b = image[:, 0, :, :], image[:, 1, :, :], image[:, 2, :, :]
-        y = 0.299 * r + 0.587 * g + 0.114 * b
-        cb = 128.0 - 0.168736 * r - 0.331264 * g + 0.5 * b
-        cr = 128.0 + 0.5 * r - 0.418688 * g - 0.081312 * b
-        return torch.stack([y, cb, cr], dim=1)
-
-    """NOT MY CODE """
-    def ycbcr_to_rgb(self, image):
-        y, cb, cr = image[:, 0, :, :], image[:, 1, :, :], image[:, 2, :, :]
-        r = y + 1.402 * (cr - 128.0)
-        g = y - 0.344136 * (cb - 128.0) - 0.714136 * (cr - 128.0)
-        b = y + 1.772 * (cb - 128.0)
-        return torch.clamp(torch.stack([r, g, b], dim=1), 0.0, 255.0)
-
-    """NOT MY CODE """
-    def match_color_new(self, source_img, target_img):
-        source_ycbcr = self.rgb_to_ycbcr(source_img)
-        target_ycbcr = self.rgb_to_ycbcr(target_img)
-        source_y, source_cb, source_cr = torch.unbind(source_ycbcr, dim=1)
-        target_y, target_cb, target_cr = torch.unbind(target_ycbcr, dim=1)
-        adjusted_ycc = torch.stack([target_y, source_cb, source_cr], dim=1)
-        adjusted_rgb = self.ycbcr_to_rgb(adjusted_ycc)
-        adjusted_rgb = (adjusted_rgb - adjusted_rgb.min()) / (adjusted_rgb.max() - adjusted_rgb.min())
-        return adjusted_rgb
-
     def forward_sequence(self, frames):
         # extract edges using LAPLACIAN/SOBEL/BOTH
+        #[0,1]
+        frames = self.edge_detector.normalize(frames)
         extracted_edges_repeat, edges_gray = self.edge_detector(frames)
         # feature learning
         residual_x = self.encoder(extracted_edges_repeat)
@@ -317,10 +496,8 @@ class EdgeEnhancement(nn.Module):
         x_frame = self.upscale_2(x_frame)
         enhanced_edges = self.conv_final(x_frame)
 
-        enhanced_edges = self.edge_detector.normalize(enhanced_edges)
-        enhanced_edges = enhanced_edges - edges_gray
-        x_super_r = self.edge_detector.normalize(enhanced_edges.repeat(1, 3, 1, 1) + frames)
-        x_super_r = self.match_color_new(x_super_r, frames)
+        enhanced_edges = self.edge_detector.normalize(enhanced_edges - extracted_edges_repeat)
+        x_super_r = self.edge_detector.normalize(enhanced_edges + frames)
         return x_super_r, enhanced_edges
 
 
@@ -332,14 +509,14 @@ def display_frame(frame):
     image_array = np.squeeze(frame.clone().detach().cpu().numpy())
     image_array = np.transpose(image_array, (1, 2, 0))
     image_array = (image_array * 255).astype(np.uint8)
-    cv2.imwrite('/vsr/sr_mine.jpg', cv2.cvtColor(image_array, cv2.COLOR_RGB2BGR))
+    cv2.imwrite('/root/vsr/mine.jpg', cv2.cvtColor(image_array, cv2.COLOR_RGB2BGR))
 
 
 def display_edges(edges):
     image_array = edges.clone().detach().cpu().numpy()
     image_array = np.transpose(image_array, (1, 2, 0))
     image_array = (image_array * 255).astype(np.uint8)
-    cv2.imwrite('/vsr/my_edges_laplacian.jpg', image_array)
+    cv2.imwrite('/root/vsr/my_edges_laplacian_base.jpg', image_array)
 
 
 if __name__ == '__main__':
@@ -353,10 +530,10 @@ if __name__ == '__main__':
     # opt["device"] = "cuda"
     # opt["dist"] = "false"
     # opt["is_train"] = "false"
-    model = EdgeEnhancement()
+    model = EdgeEnhancement(edge_enhancement_type="CannyFilter")
 
     # Move the model to GPU
-    device = torch.device("cuda:city" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model.to(device)
     opt = torch.optim.Adam(model.parameters())
 
